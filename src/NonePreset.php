87171761e0809b518421b7c1f9db5cdfd8c488a8
Nick de Kruijk <git@nickdekruijk.nl>
2020-05-21 16:07:12 +0200

    NonePreset

---
+++ src/NonePreset.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace NickDeKruijk\LaravelUIPresets;
+
+use Illuminate\Filesystem\Filesystem;
+use Laravel\Ui\Presets\Preset;
+
+class NonePreset extends Preset
+{
+    public static function install()
+    {
+        $filesystem = new Filesystem();
+
+        // Empty sass directory
+        $filesystem->deleteDirectory(resource_path('sass'));
+        $filesystem->makeDirectory(resource_path('sass'));
+
+        // Empty js directory
+        $filesystem->deleteDirectory(resource_path('js'));
+        $filesystem->makeDirectory(resource_path('js'));
+
+        // Delete Laravel default welcome view
+        $filesystem->delete(resource_path('views/welcome.blade.php'));
+
+        // Delete Laravel default welcome route
+        PresetsServiceProvider::updateFile(base_path('routes/web.php'), function ($file) {
+            return str_replace("Route::get('/', function () {\n    return view('welcome');\n});\n", "", $file);
+        });
+    }
+}
--- src/PresetsServiceProvider.php
+++ src/PresetsServiceProvider.php
@@ -14,9 +14,23 @@ class PresetsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        UiCommand::macro('preset-none', function ($command) {
+            NonePreset::install();
+            $command->info('All preset scaffolding removed successfully.');
+        });
         UiCommand::macro('preset-sections', function ($command) {
             SectionsPreset::install();
             $command->info('Sections preset scaffolding installed successfully.');
         });
+    }
+
+    /**
+     * Update the contents of a file with the logic of a given callback.
+     */
+    public static function updateFile(string $path, callable $callback)
+    {
+        $originalFileContents = file_get_contents($path);
+        $newFileContents = $callback($originalFileContents);
+        file_put_contents($path, $newFileContents);
     }
 }
